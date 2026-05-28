0d23d7ea7f1403b109bbecc54fb3a1f8592629cc
xd-001 <734954695@qq.com>
2026-05-28 19:13:37 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
+import sys
+import os
+# 将当前文件所在目录加入 sys.path，以便绝对导入
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 import pystray
 from PIL import Image
 import threading
-import sys
-import os
 import keyboard
-from .wallpaper_manager import WallpaperManager
-from .config_manager import ConfigManager
-from .settings_window import open_settings
-from .gallery_window import open_gallery
+from wallpaper_manager import WallpaperManager
+from config_manager import ConfigManager
+from settings_window import open_settings
+from gallery_window import open_gallery
 
 class TrayApp:
     def __init__(self):
@@ -20,7 +23,6 @@ class TrayApp:
     def register_hotkey(self):
         mods = self.config.get("hotkey", "hotkey_modifiers")
         key = self.config.get("hotkey", "hotkey_key")
-        # keyboard 要求组合键如 ctrl+shift+w
         hotkey = '+'.join(mods + [key])
         keyboard.add_hotkey(hotkey, self.manual_switch)
     
@@ -32,7 +34,6 @@ class TrayApp:
         self.config.set("general", "guest_mode", not current)
     
     def toggle_auto_start(self):
-        # 通过注册表设置开机启动
         import winreg
         key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
         exe_path = sys.argv[0]
@@ -46,21 +47,7 @@ class TrayApp:
                 except:
                     pass
     
-    def create_tray_menu(self):
-        return pystray.Menu(
-            pystray.MenuItem("立即切换", self.manual_switch),
-            pystray.MenuItem("喜欢当前壁纸", lambda: self.like_current()),
-            pystray.MenuItem("不喜欢当前壁纸", lambda: self.dislike_current()),
-            pystray.MenuItem("开机启动", self.toggle_auto_start, checked=lambda item: self.config.get("general", "auto_start")),
-            pystray.MenuItem("访客模式", self.toggle_guest_mode, checked=lambda item: self.config.get("general", "guest_mode")),
-            pystray.MenuItem("图库浏览", lambda: threading.Thread(target=open_gallery, args=(self.config,), daemon=True).start()),
-            pystray.MenuItem("设置", lambda: threading.Thread(target=open_settings, args=(self.config, self.manager), daemon=True).start()),
-            pystray.MenuItem("使用说明", self.show_help),
-            pystray.MenuItem("退出", self.quit_app)
-        )
-    
     def like_current(self):
-        # 获取当前活动的显示器壁纸（取最后切换的那个）
         last_monitor = self.manager.monitors[self.manager.current_monitor_index]
         self.manager.classify_current_wallpaper(last_monitor, 'like')
     
@@ -78,8 +65,26 @@ class TrayApp:
         self.icon.stop()
         sys.exit(0)
     
+    def create_tray_menu(self):
+        return pystray.Menu(
+            pystray.MenuItem("立即切换", self.manual_switch),
+            pystray.MenuItem("喜欢当前壁纸", self.like_current),
+            pystray.MenuItem("不喜欢当前壁纸", self.dislike_current),
+            pystray.MenuItem("开机启动", self.toggle_auto_start, checked=lambda item: self.config.get("general", "auto_start")),
+            pystray.MenuItem("访客模式", self.toggle_guest_mode, checked=lambda item: self.config.get("general", "guest_mode")),
+            pystray.MenuItem("图库浏览", lambda: threading.Thread(target=open_gallery, args=(self.config,), daemon=True).start()),
+            pystray.MenuItem("设置", lambda: threading.Thread(target=open_settings, args=(self.config, self.manager), daemon=True).start()),
+            pystray.MenuItem("使用说明", self.show_help),
+            pystray.MenuItem("退出", self.quit_app)
+        )
+    
     def run(self):
-        image = Image.open("icon.ico")  # 确保有图标文件
+        # 尝试加载图标，如果不存在则创建一个空白图标
+        if os.path.exists("icon.ico"):
+            image = Image.open("icon.ico")
+        else:
+            # 创建一个 64x64 的空白图片
+            image = Image.new('RGB', (64, 64), color='gray')
         self.icon = pystray.Icon("wallpaper_changer", image, "壁纸自动更换", self.create_tray_menu())
         self.icon.run()
 
